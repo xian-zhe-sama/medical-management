603aabe756e9d2bc58bf4fe4df4f5077fb512841
ES <29315@Arutoria.com>
2023-12-22 19:17:33 +0800

    创建项目

---
+++ src/main/java/org/es/medicalmanagementbackend/MedicalManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package org.es.medicalmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalManagementBackendApplication.class, args);
+    }
+
+}
